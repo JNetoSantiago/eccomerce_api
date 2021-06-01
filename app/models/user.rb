92220410387b47daf75e85d8633a7ba5be65f24c
Joao Neto <joao.developer29@gmail.com>
2021-06-01 17:29:32 -0300

    setup bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_secure_password
+  
+  # validations
   validates :email, uniqueness: true
   validates :email, email: true
   validates :password_digest, presence: true
