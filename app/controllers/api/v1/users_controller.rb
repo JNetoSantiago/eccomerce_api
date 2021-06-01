60c615f7dece549d45857b46dea0973db09fee8f
Joao Neto <joao.developer29@gmail.com>
2021-06-01 17:54:00 -0300

    adding tests for create user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,6 +5,22 @@ module Api
       def show
         render json: User.find(params[:id])
       end
+
+      # POST users
+      def create
+        @user = User.new user_params
+
+        if @user.save
+          render json: @user, status: :created
+        else
+          render json: @user.errors, status: :unprocessable_entity
+        end
+      end
+
+      private
+      def user_params
+        params.require(:user).permit(:email, :password)
+      end
     end
   end
 end
--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -13,6 +13,16 @@ module Api
         json_response = JSON.parse(self.response.body)
         assert_equal @user.email, json_response['email']
       end
+
+      test "should be able to create a user" do
+        assert_difference "User.count" do
+          post api_v1_users_url, 
+          params: { user: { email: 'so_and_so@email.com', password: '123456' } },
+          as: :json
+        end
+
+        assert_response :created
+      end
     end
   end
 end
