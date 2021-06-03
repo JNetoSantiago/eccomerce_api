e3b841d40df376e30d4b6640390f36dab60b32b9
Joao Neto <joao.developer29@gmail.com>
2021-06-03 15:31:38 -0300

    adding tests for products model

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -6,4 +6,29 @@ class ProductTest < ActiveSupport::TestCase
     product.price = -1
     assert_not product.valid?
   end
+
+  test 'product with invalid title should be invalid' do
+    product = Product.new title: nil, price: 120.90, published: true, user_id: users(:one).id, category_id: categories(:one).id
+    assert_not product.valid?
+  end
+
+  test 'product with invalid price should be invalid' do
+    product = Product.new title: 'New Product', price: nil, published: true, user_id: users(:one).id, category_id: categories(:one).id
+    assert_not product.valid?
+  end
+
+  test 'product with invalid published should be invalid' do
+    product = Product.new title: 'New Product', price: 120.90, published: nil, user_id: users(:one).id, category_id: categories(:one).id
+    assert_not product.valid?
+  end
+
+  test 'product with invalid user_id should be invalid' do
+    product = Product.new title: 'New Product', price: 120.90, published: true, user_id: nil, category_id: categories(:one).id
+    assert_not product.valid?
+  end
+
+  test 'product with invalid category_id should be invalid' do
+    product = Product.new title: 'New Product', price: 120.90, published: true, user_id: users(:one).id, category_id: nil
+    assert_not product.valid?
+  end
 end
