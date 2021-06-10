747360b6602306c483765b23d60479c53a0ffdb1
Joao Neto <joao.developer29@gmail.com>
2021-06-10 20:24:06 -0300

    decrementing product

--- app/models/placement.rb
+++ app/models/placement.rb
@@ -1,4 +1,12 @@
 class Placement < ApplicationRecord
+  # relashionships
   belongs_to :order
   belongs_to :product, inverse_of: :placements
+
+  # callbacks
+  after_create :decrement_product_quantity!
+
+  def decrement_product_quantity!
+    product.decrement!(:quantity, quantity)
+  end
 end
